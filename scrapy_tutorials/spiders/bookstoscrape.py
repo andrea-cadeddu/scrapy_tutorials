ccca4e49f3c3010ecf8dcbacb31a90f83e2c30a2
andrea-cadeddu <andrea.cadeddu@linkalab.it>
2021-04-02 14:48:30 +0200

    first commit

---
+++ scrapy_tutorials/spiders/bookstoscrape.py
@@ -0,0 +1,27 @@
+from scrapy import Spider, Request
+
+class BooksSpider(Spider):
+    name = "books"
+    start_urls = ["http://books.toscrape.com"]
+
+    def parse(self, response):
+        for entry in response.css("article.product_pod"):
+            title = entry.css("h3 a::attr(title)").get()
+            url = entry.css("a::attr(href)").get()
+            stars = entry.css("p.star-rating::attr(class)").get()
+            price = entry.css("p.price_color::text").get()
+            in_stock = entry.css("p.instock::text").extract()[1].strip()
+            print(in_stock)
+            yield {
+                "title": title,
+                "url": url,
+                "stars": stars,
+                "price": price,
+                "in_stock": in_stock
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield Request(url=next_page_url, callback=self.parse)
